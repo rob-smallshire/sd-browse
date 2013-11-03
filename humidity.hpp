e9f2bd958e29de2d4822fb0ecf1fb42eb4c61e00
Robert Smallshire <robert@smallshire.org.uk>
2013-11-03 13:28:21 +0100

    Adds support for DHT22 humidity sensor

---
+++ humidity.hpp
@@ -0,0 +1,15 @@
+/*
+ * humidity.hpp
+ *
+ *  Created on: 3 Nov 2013
+ *      Author: rjs
+ */
+
+#ifndef HUMIDITY_HPP_
+#define HUMIDITY_HPP_
+
+
+void reportHumidity();
+
+
+#endif /* HUMIDITY_HPP_ */
---
+++ temperature.hpp
@@ -0,0 +1,15 @@
+/*
+ * temperature.hpp
+ *
+ *  Created on: 3 Nov 2013
+ *      Author: rjs
+ */
+
+#ifndef TEMPERATURE_HPP_
+#define TEMPERATURE_HPP_
+
+
+void reportTemperatures();
+
+
+#endif /* TEMPERATURE_HPP_ */
